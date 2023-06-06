c1a4d0ab30ea60cd4511472ff88201e7bdd0751f
Pablo Venturino <pablojventurino@gmail.com>
2023-06-06 14:03:58 -0700

    add lightning-input on helloWebComponent

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWebComponent extends LightningElement {}
+export default class HelloWebComponent extends LightningElement {
+    greeting = 'Trailblazer';
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
+}
